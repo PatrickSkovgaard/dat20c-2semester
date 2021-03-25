89f3dfae1075900af1ec0d8f3b8ec2c645fdaa17
Nicklas <nifr@kea.dk>
2021-03-25 13:31:16 +0100

    First

--- jdbc-basic/src/main/java/JDBCConnectionTest.java
+++ jdbc-basic/src/main/java/JDBCConnectionTest.java
@@ -3,25 +3,29 @@ import java.sql.*;
 public class JDBCConnectionTest {
     //We are not "really" handling the exception for now
     public static void main(String[] args) {
-        //Create a connection to the database
-        //Create a PreparedStatement
-        //Prepared Statement contains a query and returns the rows from DB
-        //Iterate through the ResultSet
-        //Process the data
-
         try {
+            //Create a connection to the database
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_company", "dean", "securePassword");
+
+            //Create a PreparedStatement
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM departments");
+
+            //Prepared Statement contains a query and returns the rows from DB
             ResultSet rs = stmt.executeQuery();
+
+            //Iterate through the ResultSet
             while(rs.next()){
-                System.out.println(rs.getString(2));
+                int deptNo = rs.getInt(1);
+                String dName = rs.getString(2);
+                String location = rs.getString(3);
+
+                Department tmpDepartment = new Department(deptNo,dName,location);
             }
         }
         catch(SQLException e){
             System.out.println("Something went wrong");
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
-        //Create a Statement with a query
-        //Read the ResultSet
+        //Process the data
     }
 }
